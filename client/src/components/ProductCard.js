0a06453abf038506ad73e81c9a1e7887e27c3c38
RiskySaptra <kiki014045@gmail.com>
2020-04-13 14:29:03 +0700

    empty check

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -17,6 +17,7 @@ const ProductCard = observer((store) => {
   const { dataProduct, loading, source, getDataLocal } = store.productStore;
 
   if (loading === false) {
+<<<<<<< refs/remotes/origin/MobX-integration
     return (
       <>
         <Grid
@@ -59,41 +60,93 @@ const ProductCard = observer((store) => {
                       <Typography variant="body2" gutterBottom>
                         {item.productName}
                       </Typography>
+=======
+    if (data.length === 0) {
+      return (
+        <>
+          <p>Empty :(</p>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <Grid
+            container
+            direction="row"
+            justify="center"
+            alignItems="center"
+            spacing={5}
+          >
+            {data.map((item) => (
+              <Paper className={classes.paper} key={item.id}>
+                <Grid
+                  container
+                  direction="column"
+                  justify="flex-start"
+                  alignItems="stretch"
+                  wrap="nowrap"
+                  style={{ height: "100%" }}
+                >
+                  <Grid item xs={7} style={{ maxWidth: "100%" }}>
+                    <Grid className={classes.image}>
+                      <img
+                        className={classes.img}
+                        alt={item.productName}
+                        src={item.image}
+                      />
+>>>>>>> empty check
                     </Grid>
-                    <Grid item style={{ maxWidth: "100%" }}>
-                      <Grid container direction="column">
-                        <Grid item style={{ marginBottom: "5px" }}>
-                          <Typography variant="body2" gutterBottom>
-                            {toRupiah(item.price, false)}
-                          </Typography>
-                          <DetailsDialog data={item.productDetails} />
-                        </Grid>
-                        {source === "local" ? (
-                          <Grid item>
-                            <Grid container>
-                              <ProductDelete
-                                data={item}
-                                getDataLocal={getDataLocal}
-                              />
-                              <ProductEdit
-                                data={item}
-                                getDataLocal={getDataLocal}
-                              />
-                            </Grid>
+                  </Grid>
+                  <Grid item xs={5} style={{ maxWidth: "100%" }}>
+                    <Grid
+                      container
+                      direction="column"
+                      justify="space-between"
+                      style={{ padding: "5px", minHeight: "100%" }}
+                    >
+                      <Grid item style={{ maxWidth: "100%" }}>
+                        <Typography variant="body2" color="textSecondary">
+                          SKU : {item.SKU}
+                        </Typography>
+                        <Typography variant="body2" gutterBottom>
+                          {item.productName}
+                        </Typography>
+                      </Grid>
+                      <Grid item style={{ maxWidth: "100%" }}>
+                        <Grid container direction="column">
+                          <Grid item style={{ marginBottom: "5px" }}>
+                            <Typography variant="body2" gutterBottom>
+                              {toRupiah(item.price, false)}
+                            </Typography>
+                            <DetailsDialog data={item.productDetails} />
                           </Grid>
-                        ) : (
-                          <></>
-                        )}
+                          {source === "local" ? (
+                            <Grid item>
+                              <Grid container>
+                                <ProductDelete
+                                  data={item}
+                                  getDataLocal={getDataLocal}
+                                />
+                                <ProductEdit
+                                  data={item}
+                                  getDataLocal={getDataLocal}
+                                />
+                              </Grid>
+                            </Grid>
+                          ) : (
+                            <></>
+                          )}
+                        </Grid>
                       </Grid>
                     </Grid>
                   </Grid>
                 </Grid>
-              </Grid>
-            </Paper>
-          ))}
-        </Grid>
-      </>
-    );
+              </Paper>
+            ))}
+          </Grid>
+        </>
+      );
+    }
   } else {
     return (
       <>
